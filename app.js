a3d8a3b228b33341e8bf241d9c8c2c45846f4113
Nader PJR <148011035+naderpjr@users.noreply.github.com>
2024-02-23 18:04:52 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const faqQues = document.querySelectorAll('.faq-question');
 const faqAnswer = document.querySelectorAll('.faq-answer');
 const plus = document.querySelectorAll('.plus-img');
-const title = document.querySelectorAll('h3');
 
 faqQues.forEach((faqQuestion, index) => {
   faqQuestion.addEventListener('click', () => {
@@ -9,11 +8,9 @@ faqQues.forEach((faqQuestion, index) => {
       if (i === index) {
         answer.classList.add('active');
         plus[i].src = './assets/images/icon-minus.svg';
-        title[i].classList.add('active');
       } else {
         answer.classList.remove('active');
         plus[i].src = './assets/images/icon-plus.svg';
-        title[i].classList.remove('active');
       }
     });
   });
